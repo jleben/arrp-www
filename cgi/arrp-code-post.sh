7fd5db9406df3d0f523fb69b40b1e148bede13e0
Jakob Leben <jakob.leben@gmail.com>
2016-08-23 16:13:48 -0700

    cgi: execute in a dedicated directory

--- cgi/arrp-code-post.sh
+++ cgi/arrp-code-post.sh
@@ -1,18 +1,14 @@
-
 #!/bin/bash
 
-echo "Content-type: text/html"
-echo
+cd ../requests
+root_dir="../"
 
-if [ -z $arrp ]
-then
-  echo "Environment variable 'arrp' undefined"
-  exit 0
-fi
+echo "Content-type: text/plain"
+echo
 
 tee > code.arrp
 
-$arrp code.arrp --cpp kernel --cpp-namespace kernel 2> errors.log
+$root_dir/arrp/arrp code.arrp --cpp kernel --cpp-namespace kernel 2> errors.log
 
 if [ $? != 0 ]
 then
@@ -20,7 +16,7 @@ then
   exit 0
 fi
 
-c++ -std=c++11 -I../arrp -I. ../arrp/generic_main.cpp -o program 2> errors.log
+c++ -std=c++11 -I$root_dir/arrp -I. $root_dir/arrp/generic_main.cpp -o program 2> errors.log
 
 if [ $? != 0 ]
 then
