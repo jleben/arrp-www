a3d874e99f4186e6938145f764f56b475a5e3b6e
Jakob Leben <jakob.leben@gmail.com>
2016-08-25 09:02:30 -0700

    cgi: compile c++ with debug symbols, no optimization

--- cgi/arrp-code-post.sh
+++ cgi/arrp-code-post.sh
@@ -40,7 +40,7 @@ then
   exit 0
 fi
 
-c++ -std=c++11 -I$root_dir/arrp -I. $root_dir/arrp/generic_main.cpp -o program 2> errors.log
+c++ -std=c++11 -g -O0 -I$root_dir/arrp -I. $root_dir/arrp/generic_main.cpp -o program 2> errors.log
 
 if [ $? != 0 ]
 then
