2f660398205f95ff5618470f0e3fb3e66730c542
Jakob Leben <jakob.leben@gmail.com>
2016-09-01 16:37:15 -0700

    update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 dest="$1"
+url_prefix="$2"
 
-if [[ -z "$dest" ]]; then
-  echo "Missing argument: Destination."
+if [[ -z "$dest" || -z "$url_prefix" ]]; then
+  echo "** ERROR: Required arguments: <destination> <url prefix>"
   exit 1
 fi
 
@@ -12,12 +13,27 @@ cp "arrp/generic_main.cpp" "arrp/generic_printer.hpp" "$dest/arrp"
 
 mkdir -p "$dest/cgi"
 cp "cgi/arrp-code-post.sh" "$dest/cgi"
+cp "cgi/arrp-code-post-entry.sh" "$dest/cgi"
 
-mkdir -p "$dest/content/html"
-cp "content/html/index.html" "$dest/content/html"
+mkdir -p "$dest/html"
+cp "html/index.html" "$dest/html"
 
-mkdir -p "$dest/content/cgi"
-cp "content/cgi/arrp-code-post-entry.sh" "$dest/content/cgi"
+mkdir -p "$dest/apache"
+
+function escape_sed_replacement {
+  echo "$1" | sed -e "s/[/]/\\\&/g" -e "s/[.]/\\\&/g"
+}
+
+url_prefix_repl=$(escape_sed_replacement "$url_prefix")
+file_prefix_repl=$(escape_sed_replacement "$dest")
+
+echo "$url_prefix_repl"
+echo "$file_prefix_repl"
+
+sed \
+  -e "s/{{url\-prefix}}/$url_prefix_repl/g" \
+  -e "s/{{file\-prefix}}/$file_prefix_repl/g" \
+  apache/arrp.conf.template > "$dest/apache/arrp.conf"
 
 mkdir -p "$dest/requests"
 chmod 777 "$dest/requests"
